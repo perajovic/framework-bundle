ec66983f1f34c7268ea94644504339522fb8f13d
Pera Jovic <pera@codecontrol.rs>
2014-10-25 11:28:20 +0200

    Moved sharable things into TestCaseTrait

--- src/Codecontrol/FrameworkBundle/Test/TestCase.php
+++ src/Codecontrol/FrameworkBundle/Test/TestCase.php
@@ -3,23 +3,14 @@
 namespace Codecontrol\FrameworkBundle\Test;
 
 use \PHPUnit_Framework_TestCase as PHPUnit_Framework_TestCase;
-use \ReflectionClass as ReflectionClass;
-use \ReflectionObject as ReflectionObject;
 
 abstract class TestCase extends PHPUnit_Framework_TestCase
 {
+    use TestCaseTrait;
+
     protected function tearDown()
     {
-        $rObj = new ReflectionObject($this);
-
-        foreach ($rObj->getProperties() as $property) {
-            if (!$property->isStatic() && 0
-                !== strpos($property->getDeclaringClass()->getName(), 'PHPUnit_')
-            ) {
-                $property->setAccessible(true);
-                $property->setValue($this, null);
-            }
-        }
+        $this->nullifyProperties();
 
         parent::tearDown();
     }
@@ -37,67 +28,5 @@ abstract class TestCase extends PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->setMethods($methods)
             ->getMock();
-    }
-
-    /**
-     * @param object $obj
-     * @param string $method
-     * @param array  $arguments
-     *
-     * @return mixed
-     */
-    protected function callNonPublicMethodWithArguments(
-        $obj,
-        $method,
-        array $arguments = []
-    ) {
-        return $this
-            ->setClassMemberAsAccessible($obj, $method, 'method')
-            ->invokeArgs($obj, $arguments);
-    }
-
-    /**
-     * @param mixed  $obj
-     * @param string $property
-     * @param mixed  $value
-     *
-     * @return mixed
-     */
-    protected function setNonPublicPropertyValue($obj, $property, $value)
-    {
-        $this
-            ->setClassMemberAsAccessible($obj, $property, 'property')
-            ->setValue($obj, $value);
-    }
-
-    /**
-     * @param mixed  $obj
-     * @param string $property
-     *
-     * @return mixed
-     */
-    protected function getNonPublicPropertyValue($obj, $property)
-    {
-        return $this
-            ->setClassMemberAsAccessible($obj, $property, 'property')
-            ->getValue($obj);
-    }
-
-    /**
-     * @param mixed  $classOrObj
-     * @param string $member
-     * @param string $type
-     *
-     * @return mixed
-     */
-    protected function setClassMemberAsAccessible($classOrObj, $member, $type)
-    {
-        $class = is_object($classOrObj) ? get_class($classOrObj) : $classOrObj;
-        $method = 'method' === $type ? 'getMethod' : 'getProperty';
-
-        $member = (new ReflectionClass($class))->{$method}($member);
-        $member->setAccessible(true);
-
-        return $member;
     }
 }
