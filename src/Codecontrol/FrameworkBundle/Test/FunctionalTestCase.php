b3d4c00d2f6ecbc99ebb929f6fd0c16297973a44
Pera Jovic <pera@codecontrol.rs>
2014-10-29 11:11:50 +0100

    Added automated truncating tables feature in FunctionalTestCase

--- src/Codecontrol/FrameworkBundle/Test/FunctionalTestCase.php
+++ src/Codecontrol/FrameworkBundle/Test/FunctionalTestCase.php
@@ -3,6 +3,8 @@
 namespace Codecontrol\FrameworkBundle\Test;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use \LogicException as LogicException;
+use \RuntimeException as RuntimeException;
 
 abstract class FunctionalTestCase extends WebTestCase
 {
@@ -18,6 +20,7 @@ abstract class FunctionalTestCase extends WebTestCase
 
     protected function tearDown()
     {
+        $this->truncateTables();
         $this->nullifyProperties();
 
         parent::tearDown();
@@ -33,5 +36,59 @@ abstract class FunctionalTestCase extends WebTestCase
     protected function getService($id)
     {
         return static::$kernel->getContainer()->get($id);
+    }
+
+    /**
+     * @return Doctrine\ORM\EntityManagerInterface
+     *
+     * @throws LogicException
+     * @throws RuntimeException
+     */
+    protected function getEntityManager()
+    {
+        if (!static::$kernel->getContainer()->has('doctrine')) {
+            throw new LogicException(
+                'The DoctrineBundle is not registered in your application.'
+            );
+        }
+
+        if (!defined('static::EM_ID')) {
+            throw new RuntimeException(sprintf(
+                'Constant %s::EM_ID is not defined.',
+                get_class($this)
+            ));
+        }
+
+        return $this->getService('doctrine')->getManager(static::EM_ID);
+    }
+
+    private function truncateTables()
+    {
+        $container = static::$kernel->getContainer();
+        if (!$container->has('codecontrol_framework.listener.truncatable_tables')) {
+            return;
+        }
+
+        $tables = $container
+            ->get('codecontrol_framework.listener.truncatable_tables')
+            ->getTables();
+
+        if (empty($tables)) {
+            return;
+        }
+
+        $connection = $this->getEntityManager()->getConnection();
+
+        if ($connection->getDatabasePlatform()->getName() !== 'postgresql') {
+            return;
+        }
+
+        foreach ($tables as $table) {
+            $connection->exec(sprintf('TRUNCATE %s CASCADE', $table));
+            $connection->exec(sprintf(
+                'ALTER SEQUENCE %s_id_seq RESTART WITH 1',
+                $table
+            ));
+        }
     }
 }
