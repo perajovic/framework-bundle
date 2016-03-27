180172b9b2550ea1644af144ba851ae8d09a83de
Pera Jovic <perajovic@me.com>
2016-03-27 13:52:27 +0200

    Add license and strict definition

--- src/Filos/FrameworkBundle/Entity/UpdatableTrait.php
+++ src/Filos/FrameworkBundle/Entity/UpdatableTrait.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the Filos framework.
+ *
+ * (c) Pera Jovic <perajovic@me.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types = 1);
+
 namespace Filos\FrameworkBundle\Entity;
 
 use DateTime;
