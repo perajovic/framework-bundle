afe0ef4221812bc9c1b1036ed2933c2415050375
Pera Jovic <perajovic@me.com>
2016-10-22 16:39:23 +0200

    Add default config for tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This file is part of the Filos FrameworkBundle project.
+ *
+ * (c) Pera Jovic <perajovic@me.com>. All rights reserved.
+ */
+
+error_reporting(-1);
+mb_internal_encoding('UTF-8');
+date_default_timezone_set('UTC');
+
+include __DIR__.'/../vendor/autoload.php';
