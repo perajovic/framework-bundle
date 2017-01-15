0f8bd5f55a4d7968bdc10738f921e7ae4bc11ab2
Pera Jovic <perajovic@me.com>
2017-01-15 11:37:34 +0100

    Run CS fixer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  * (c) Pera Jovic <perajovic@me.com>. All rights reserved.
-*/
+ */
 
 declare(strict_types=1);
 
